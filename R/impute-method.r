3dcb3f309d44ea77c9f19f4f288bc0519b78d87f
Tom Liptrot <tom@ortom.co.uk>
2019-05-01 09:12:04 +0000

    added test and documentation

--- R/impute-method.r
+++ R/impute-method.r
@@ -1,26 +1,45 @@
 
+#' Simple Imputation
+#'
+#' Generic simple imputation method
+#' @param x Numeric, Factor, Logical or Charater vector, or a Data Frame
+#' @param ... arugments to be passed to othermethods
+#'
+#' @return
+#' @export
+#'
+#' @examples
 impute <- function (x, ...) {
   UseMethod("impute", x)
 }
 
-
+#' @describeIn impute Median Imputation for a numeric Vector
+#' @param x
+#' @param .fun a fuction (default median) to use for imputation
 impute.numeric = function(x, .fun = median,...){
   x[is.na(x)] = .fun(x, na.rm = TRUE,...)
   x
 }
+
+#' @describeIn impute Mode Imputation for a character vector
 impute.character = function(x){
   tt = table(x)
-  mode = names(sort(x))[1]
+  mode = names(sort(tt, decreasing = TRUE))[1]
   x[is.na(x)] = mode
   x
 }
+
+#' @describeIn impute Mode Imputation for a factor vector
 impute.factor = function(x){
   impute.character(x)
 }
+
+#' @describeIn impute Mode Imputation for a logical vector
 impute.logical = function(x){
-  impute.character(x)
+  as.logical(impute.character(x))
 }
 
+#' @describeIn impute data frame imputation
 impute.data.frame = function(x, ...){
-  map_df(x, impute, ...)
+  purrr::map_df(x, impute, ...)
 }
--- tests/testthat/test-impute-method.r
+++ tests/testthat/test-impute-method.r
@@ -2,8 +2,41 @@ context("impute s3 methods")
 library(simpute)
 library(testthat)
 
-x = c(1,NA,2,3,NA)
+
+impute(test_data)
 
 test_that("numeric median imputation works", {
-  expect_equal(impute.numeric(x), c(1,2,2,3,2))
+  expect_equal(impute(c(1,NA,2,3,NA)), c(1,2,2,3,2))
 })
+
+test_that("character mode imputation works", {
+  expect_equal(impute(c('a', 'a', NA, 'b')), c('a', 'a', 'a', 'b'))
+  #to do - how to deal with ties??
+})
+
+test_that("factor mode imputation works", {
+  fac = as.factor(c('b', 'b', NA, 'a'))
+  fac2 = as.factor(c('b', 'b', 'b', 'a'))
+  expect_equal(impute.factor(fac), fac2 )
+  expect_equal(impute.factor(factor(fac, ordered = TRUE)), factor(fac2, ordered = TRUE) )
+})
+
+test_that("logical mode imputation works", {
+  expect_equal(impute(c(TRUE, TRUE, FALSE, NA, NA)), c(TRUE, TRUE, FALSE, TRUE, TRUE) )
+})
+
+test_that("data frame imputation works", {
+  test_data = dplyr::data_frame(num = c(1,NA,2,3,NA),
+                         char = c('a', 'a', NA, 'b', NA),
+                         fac = as.factor(c('b', 'b', NA, 'a', NA)),
+                         logical = c(TRUE, TRUE, FALSE, NA, NA))
+  test_data_complete = dplyr::data_frame(num = c(1,2,2,3,2),
+                         char = c('a', 'a', 'a', 'b', 'a'),
+                         fac = as.factor(c('b', 'b', 'b', 'a', 'b')),
+                         logical = c(TRUE, TRUE, FALSE, TRUE, TRUE))
+  expect_equal(impute.data.frame(test_data), test_data_complete )
+})
+
+impute.data.frame(test_data)
+
+
